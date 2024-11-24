d85fc794d5e1801c436dc83b8587adffbb5fd161
Gustavo <gustavolopesoliveira285@gmail.com>
2024-11-24 19:23:25 -0300

    Bonus

--- Codeforces/QuestoesAleatorios/martelo.cpp
+++ Codeforces/QuestoesAleatorios/martelo.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int modulo(int n){
+
+    if(n < 0){
+        return -1 * n;
+    } else {
+        return n;
+    }
+}
+
 int main(){
     //Objetivo,Muralha,Martelo
     int x,y,z;
@@ -10,7 +19,11 @@ int main(){
     int movMin = 0;
     bool condicao = false;
 
-    if()
+    if(modulo(z) < modulo(y)){
+        condicao = true;
+        
+
+    }
 
     if(condicao){
         cout << movMin << endl;
---
+++ Codeforces/QuestoesAleatorios/prova.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int l,y;
+    cin >> l >> y;
+
+    int l1,l2,l4;
+
+    if()
+
+    int resultado = 0;
+
+
+    cout << resultado << endl;
+}
